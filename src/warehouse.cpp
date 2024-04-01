40870669e2b8e7026a6bf964b981cb12aaaf261a
Eldemelo <bluehawk36000@gmail.com>
2024-03-31 21:39:11 -0400

    Added code to read from a file and create a vector of objects for parsing data

--- src/warehouse.cpp
+++ src/warehouse.cpp
@@ -1,5 +1,9 @@
 #include "warehouse.h"
 #include "order.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -8,5 +12,16 @@ warehouse::warehouse(){
 }
 
 warehouse::warehouse(string fName){
-    
+    vector<Order> orders;
+    // Read from the file
+    ifstream inFile(fName);
+    string currLine;
+    while(getline(inFile, currLine)){
+        stringstream ss(currLine);
+        string clientName;
+        int orderId, orderSize;
+        ss >>clientName >> orderId, orderSize;
+        Order newOrder(orderId, orderSize,clientName);
+        orders.push_back(newOrder);
+    }
 }

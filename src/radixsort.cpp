c708d8fff522f4b661382b9107c67267abe59aeb
ChrisJiro <christian_lopez@uri.edu>
2024-04-08 00:38:38 -0400

    Order size/id to string, radixsort cpp with getMax

--- src/radixsort.cpp
+++ src/radixsort.cpp
@@ -3,33 +3,3 @@
 #include <vector>
 
 using namespace std;
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-Order* RadixSort::findMax(vector<Order*> orders) {
-    Order* maxOrder = orders[0];
-    for (Order* currOrder : orders) {
-        if (stoi(currOrder->orderSize) > stoi(maxOrder->orderSize)) {
-            maxOrder = currOrder;
-        }
-    }
-    return maxOrder;
-}
-
-void RadixSort::countSort(vector<Order*> orders) {
-
-}
-
-vector<Order*> RadixSort::radixSort(vector<Order*> orders) {
-    Order* largestOrder = findMax(orders);
-    string tempDigits = largestOrder->orderSize;
-    int digits = tempDigits.size();
-
-}
-=======
-int main(int argc, char *argv[]){
-    cout << "hi uwu";
-}
->>>>>>> f588daa (added hi)
-=======
->>>>>>> a534800 (Updated order data types and warehouse)

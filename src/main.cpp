d270fcc9941ac9b0152f65ab51ff8fe003b73e55
Christian Lopez <157417952+ChrisJiro@users.noreply.github.com>
2024-04-27 12:15:46 -0400

    Update main.cpp with API documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include "warehouse.h"
-#include <iostream>
-#include <string>
-#include <fstream>
 
-using namespace std;
-
-//Main to call upon warehouse driver code.
+/**
+ * @brief Main method to call upon warehouse driver code.
+ * 
+ * @param argc The number of CLAs
+ * @param argv The array of CLAs
+ * @return int Return 0 to successfully complete program
+ */
 int main(int argc, char *argv[]){
-    // CLA format is: (.exe) (Input file (data))
+    //CLA format is: (.exe) (Input file (data))
     string fName = argv[1];
 
     //orders.txt is Names, Order ID, Number of Items
     warehouse myWarehouse(fName);
-    // myWarehouse.printVector();
+    //Return 0 to successfully complete program
+    return 0;
 }
